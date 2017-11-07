e7d3843d13413fe5bcb3b5ffbf67cf21db3a68d1
potatoshrike <bermudezcode@yahoo.com>
2017-11-07 19:28:24 -0400

    Agregado: vue-resource

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource'
 import VueRouter from 'vue-router'
 import Rutas from './rutas'
 import 'vue-material-design-icons/styles.css'
 
+Vue.use(VueResource);
 Vue.use(VueRouter);
 
 const router = new VueRouter({
